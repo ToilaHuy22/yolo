63dd63ed9afecde9c9f8f4a6a86ad348043da4b2
Huy <xinhtupeanh@gmail.com>
2022-09-21 21:41:15 +0700

    3.1 React (Header)| Responsive for Tablet&Mobile: useRef, .toggle() Active and close menu form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,6 +29,7 @@ const Header = () => {
 
 	const headerRef = useRef(null);
 
+	//Short header when scroll
 	useEffect(() => {
 		window.addEventListener('scroll', () => {
 			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -42,6 +43,12 @@ const Header = () => {
 		};
 	}, []);
 
+	const menuLeft = useRef(null);
+
+	const menuToggle = () => {
+		menuLeft.current.classList.toggle('active');
+	};
+
 	return (
 		<div className="header" ref={headerRef}>
 			<div className="container">
@@ -51,18 +58,20 @@ const Header = () => {
 					</Link>
 				</div>
 				<div className="header__menu">
-					<div className="header__menu__mobile-toggle">
+					<div className="header__menu__mobile-toggle" onClick={menuToggle}>
 						<i class="bx bx-menu-alt-left"></i>
 					</div>
-					<div className="header__menu__left">
-						<div className="header__menu__left__close">
+					<div className="header__menu__left" ref={menuLeft}>
+						<div className="header__menu__left__close" onClick={menuToggle}>
 							<i class="bx bx-chevron-left"></i>
 						</div>
 						{mainNav.map((item, index) => (
 							<div
+								key={index}
 								className={`header__menu__item header__menu__left__item ${
 									index === activeNav ? 'active' : ''
 								}`}
+								onClick={menuToggle}
 							>
 								<Link to={item.path}>
 									<span>{item.display}</span>
